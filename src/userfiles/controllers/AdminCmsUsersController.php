5beb0a6c5cf9919d064282efaf0faa72d185374f
Syco <syco@ArchMac>
2017-04-13 13:42:04 +0000

    fix src user controller
    Signed-off-by: Syco <syco@ArchMac>

--- src/userfiles/controllers/AdminCmsUsersController.php
+++ src/userfiles/controllers/AdminCmsUsersController.php
@@ -13,30 +13,41 @@ class AdminCmsUsersController extends \crocodicstudio\crudbooster\controllers\CB
 		$this->table               = 'cms_users';
 		$this->primary_key         = 'id';
 		$this->title_field         = "name";
-		$this->button_action_style = 'button_icon';	
-		$this->button_import 	   = FALSE;	
-		$this->button_export 	   = FALSE;	
+		$this->button_action_style = "button_icon";
+		$this->button_import       = TRUE;
+		$this->button_export       = TRUE;
+		$this->limit               = "20";
+		$this->orderby             = "name,asc";
+		$this->global_privilege    = FALSE;
+		$this->button_table_action = TRUE;
+		$this->button_bulk_action  = TRUE;
+		$this->button_add          = TRUE;
+		$this->button_edit         = TRUE;
+		$this->button_delete       = TRUE;
+		$this->button_detail       = TRUE;
+		$this->button_show         = TRUE;
+		$this->button_filter       = TRUE;
 		# END CONFIGURATION DO NOT REMOVE THIS LINE
-	
+
 		# START COLUMNS DO NOT REMOVE THIS LINE
 		$this->col = array();
 		$this->col[] = array("label"=>"Name","name"=>"name");
 		$this->col[] = array("label"=>"Email","name"=>"email");
 		$this->col[] = array("label"=>"Privilege","name"=>"id_cms_privileges","join"=>"cms_privileges,name");
-		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>1);		
-		$this->col[] = array("label"=>"Custom session variables","name"=>"custom_session_variables");
+		$this->col[] = array("label"=>"Photo","name"=>"photo","image"=>TRUE);
+		$this->col[] = array("label"=>"Custom Session Variables","name"=>"custom_session_variables");
 		# END COLUMNS DO NOT REMOVE THIS LINE
 
 		# START FORM DO NOT REMOVE THIS LINE
-		$this->form = array(); 		
-		$this->form[] = array("label"=>"Name","name"=>"name",'required'=>true,'validation'=>'required|alpha_spaces|min:3');
-		$this->form[] = array("label"=>"Email","name"=>"email",'required'=>true,'type'=>'email','validation'=>'required|email|unique:cms_users,email,'.CRUDBooster::getCurrentId());		
-		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","help"=>"Recommended resolution is 200x200px",'required'=>true,'validation'=>'required|image|max:1000');											
-		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name",'required'=>true);						
+		$this->form = array();
+		$this->form[] = array("label"=>"Name","name"=>"name","type"=>"text","validation"=>"required|alpha_spaces|min:3");
+		$this->form[] = array("label"=>"Email","name"=>"email","type"=>"email","validation"=>"required|email|unique:cms_users,email,".CRUDBooster::getCurrentId());
+		$this->form[] = array("label"=>"Photo","name"=>"photo","type"=>"upload","validation"=>"image|max:1000","help"=>"Recommended resolution is 200x200px");
+		$this->form[] = array("label"=>"Privilege","name"=>"id_cms_privileges","type"=>"select","datatable"=>"cms_privileges,name");
 		$this->form[] = array("label"=>"Password","name"=>"password","type"=>"password","help"=>"Please leave empty if not change");
-		$this->form[] = array("label"=>"Custom session variables","name"=>"custom_session_variables","type"=>"text","help"=>"Please insert a list of 'key:string_value' one per line",'required'=>false,'validation'=>'string|min:0|max:5000');											
+		$this->form[] = array("label"=>"Custom Session Variables","name"=>"custom_session_variables","type"=>"textarea","validation"=>"string|min:0|max:5000","help"=>"Please insert a list of 'key:string_value' one per line");
 		# END FORM DO NOT REMOVE THIS LINE
-				
+
 	}
 
 	public function getProfile() {			
